55fe67b1499b4ff5809eb2d2959238a5124dfe8f
juanjborja <juan.borja@patagoniansys.com>
2020-09-08 19:38:18 -0300

    Get movies from OMDb api!

--- src/TPO1/concurrencia/SimpleExample.java
+++ src/TPO1/concurrencia/SimpleExample.java
@@ -12,30 +12,14 @@ import java.util.concurrent.RecursiveAction;
  * @author juan
  */
 public class SimpleExample extends RecursiveAction{
-    final int LIMIT = 40;
-    static int result;
-    int start, end;
-    int[] data;
+
+
     SimpleExample(int[] data, int start, int end) {
-		   this.start = start;
-		   this.end = end;
-		   this.data = data;
+		  
 	   }
     @Override
     protected void compute() {
-        if((end - start)< LIMIT){
-            for(int i= start;i<end;i++)result+= data[i]*data[i];   
-            
-	}
-        else {
-            int mid = (start + end)/2;
-            SimpleExample left = new SimpleExample(data, start, mid); 
-            SimpleExample right = new SimpleExample(data, mid, end);
-            left.fork();
-            right.fork();
-            left.join();
-            right.join();
-	}
+        
     }
     
 }
--- src/TPO1/concurrencia/main.java
+++ src/TPO1/concurrencia/main.java
@@ -5,18 +5,28 @@
  */
 package TPO1.concurrencia;
 
-import java.util.concurrent.ForkJoinPool;
+import static TPO1.concurrencia.utiles.getMovie;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author juan
  */
 public class main {
-    public static void main(String[] args) {
-        ForkJoinPool pool = new ForkJoinPool();
-        int [] data = {1,2,3,4,5,6,7,8,9,10,23,32};
-        SimpleExample app = new SimpleExample(data, 0, data.length);
-        pool.invoke(app);
-        System.out.println(app.result);
+    public static void main(String[] args) throws IOException {
+        String []titles = {"The Shawshank Redemption", "The Godfather", "The Godfather: Part II", "The Dark Knight", "12 Angry Men"};
+       
+        for (String title : titles) {
+             String aux = getMovie(title);
+            System.out.println(aux);
+        }
+       
+   
     }
 }
