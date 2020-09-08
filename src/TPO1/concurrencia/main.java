caf55f282f0172d0adb556dc0ba79df1add9a745
juanjborja <juan.borja@patagoniansys.com>
2020-09-08 20:00:57 -0300

    Download data and compare between concurrent/non-concurrent way

--- src/TPO1/concurrencia/main.java
+++ src/TPO1/concurrencia/main.java
@@ -11,6 +11,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,13 +21,16 @@ import java.util.logging.Logger;
  */
 public class main {
     public static void main(String[] args) throws IOException {
+        long time = System.currentTimeMillis();
         String []titles = {"The Shawshank Redemption", "The Godfather", "The Godfather: Part II", "The Dark Knight", "12 Angry Men"};
        
         for (String title : titles) {
              String aux = getMovie(title);
             System.out.println(aux);
         }
-       
-   
+        System.out.println("Tiempo no concurrente : "+(System.currentTimeMillis()-time));
+        time = System.currentTimeMillis();
+        ForkJoinPool.commonPool().invoke(new Downloader(titles));
+        System.out.println("Tiempo concurrente : "+(System.currentTimeMillis()-time));
     }
 }
