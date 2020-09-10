f505fcb889cc6e8ed5c8ca2634111e7883651fbb
Darian <darian.jorgevich@est.fi.uncoma.edu.ar>
2020-09-10 10:51:41 -0300

    Example for Chain Of Responsability

---
+++ src/TPO1/patrones/Director.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TPO1.patrones;
+
+/**
+ *
+ * @author darian
+ */
+public class Director implements Handler{
+    private Handler next;
+    
+    @Override
+    public Handler getNext(){
+        return next;
+    }
+    @Override
+    public void setNext(Handler handler){
+        next = handler;
+    }
+    @Override
+    public void request(int amount){
+        if(amount>100000){
+            System.out.println("I can handle it, Director");
+        }
+    }
+}
---
+++ src/TPO1/patrones/Handler.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TPO1.patrones;
+
+/**
+ *
+ * @author darian
+ */
+public interface Handler {
+    public void setNext(Handler handler);
+    public Handler getNext();
+    public void request(int amount);
+}
